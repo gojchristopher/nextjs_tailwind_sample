a9d6e376dd79c58241f66004ef3a33fe1c6ddeeb
John Christopher Go <go.jchristopher@gmail.com>
2023-04-25 11:58:24 +0800

    add new font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,19 +2,27 @@ import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
 	return (
-		<Html lang="en" className="scroll-smooth">
-			<Head>
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-				<link
-					rel="stylesheet"
-					href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
-				></link>
-			</Head>
-			<body className="font-sans bg-white text-neutral-900 scroll-smooth">
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
-	);
+    <Html lang="en" className="scroll-smooth">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
+      <body className="font-sans bg-white text-neutral-900 scroll-smooth">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,46 +2,47 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-	],
-	theme: {
-		extend: {
-			colors: {
-				primary: {
-					900: "#520187",
-					800: "#5E1C9E",
-					700: "#7224BF",
-					600: "#C188F9",
-					500: "#DDBBFE",
-					300: "#D6BBFB",
-					200: "#E9D7FE",
-					100: "#F4EBFF",
-					50: "#F9F5FF",
-				},
-				neutral: {
-					900: "#0F0F0F",
-					800: "#2E2E2E",
-					700: "#525252",
-					600: "#7A7A7A",
-					500: "#A3A3A3",
-					400: "#B8B8B8",
-					300: "#D6D6D6",
-					200: "#F0F0F0",
-					100: "#FCFCFC",
-				},
-			},
-			fontFamily: {
-				sans: ["'Inter'", ...defaultTheme.fontFamily.sans],
-			},
-			backgroundImage: {
-				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-				"gradient-conic":
-					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-			},
-		},
-	},
-	plugins: [],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          900: "#520187",
+          800: "#5E1C9E",
+          700: "#7224BF",
+          600: "#C188F9",
+          500: "#DDBBFE",
+          300: "#D6BBFB",
+          200: "#E9D7FE",
+          100: "#F4EBFF",
+          50: "#F9F5FF",
+        },
+        neutral: {
+          900: "#0F0F0F",
+          800: "#2E2E2E",
+          700: "#525252",
+          600: "#7A7A7A",
+          500: "#A3A3A3",
+          400: "#B8B8B8",
+          300: "#D6D6D6",
+          200: "#F0F0F0",
+          100: "#FCFCFC",
+        },
+      },
+      fontFamily: {
+        sans: ["'Inter'", ...defaultTheme.fontFamily.sans],
+        montserrat: ["Montserrat"],
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
 };
